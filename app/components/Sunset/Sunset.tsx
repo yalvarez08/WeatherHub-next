d6c20061bbc37c1d02c0b63bc612e9c198c10d35
Yailene Alvarez <yailene.alvarez@gmail.com>
2024-05-23 16:32:13 -0400

    sunset info included

--- app/components/Sunset/Sunset.tsx
+++ app/components/Sunset/Sunset.tsx
@@ -1,9 +1,29 @@
 "use client"
 import React from 'react'
+import { useGlobalContext } from '@/app/context/GlobalContext'
+import { Skeleton } from '@/components/ui/skeleton';
+import { sunset } from '@/app/utils/Icons';
 
 function Sunset() {
+    const {weather} = useGlobalContext();
+    const {current, forecast, condition, location} = weather;
+    const sunsetTime = forecast?.forecastday[0].astro.sunset;
+    const moonriseTime = forecast?.forecastday[0].astro.moonrise;
+
+
+    if (!forecast || !sunsetTime) {
+        return <Skeleton className="h-[12rem] w-full" />;
+    }
+
   return (
-    <div>Sunset</div>
+    <div className="pt-6 pb-5 px-4 h-[12rem] border rounded-lg flex flex-col gap-8 dark:bg-dark-grey shadow-sm dark:shadow-none">
+      <div className="top">
+        <h2 className="flex items-center gap-2 font-medium">{sunset}Sunset</h2>
+        <p className="pt-4 text-2xl">{sunsetTime}</p>
+      </div>
+
+      <p className="text-sm">Sunrise: {moonriseTime}</p>
+    </div>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Temperature from "./components/Temperature/Temperature";
 import AirQuality from "./components/AirQuality/AirQuality";
+import Sunset from "./components/Sunset/Sunset";
 
 const Home = () => {
   
@@ -14,8 +15,9 @@ const Home = () => {
           <Temperature />
         </div>
         <div className="flex flex-col w-full">
-          <div className="instruments grid h-full gap-4 col-span-full sm-2:col-span-2 lg:grid-cols-3 xl:grid-cols-4">
+          <div className="gadgets grid h-full gap-4 col-span-full sm-2:col-span-2 lg:grid-cols-3 xl:grid-cols-4">
             <AirQuality />
+            <Sunset />
           </div>
         </div>
       </div>
